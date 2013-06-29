1e2ccc1df0bb1bba146a39ccdd9b38eb82b24399
Viacheslav <slavanw@gmail.com>
2013-06-29 13:34:25 +0200

    IDGenerator

---
+++ WifiMeasure/src/menthallab/wifimeasure/IDGenerator.java
@@ -0,0 +1,16 @@
+package menthallab.wifimeasure;
+
+public class IDGenerator
+{
+	private static int id = 1;
+	
+	public static void init(int initValue)
+	{
+		id = initValue;
+	}
+	
+	public static int getNextId()
+	{
+		return id++;
+	}
+}
